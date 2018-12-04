46036b8be05a8b1521bc9277590804db45aa3d22
DionLegierse <dal.legierse@student.avans.nl>
2018-12-04 14:59:52 +0100

    Update - app - started with javascript functions for bridge control

---
+++ Theapp/app/src/main/assets/bridge/bridge.js
@@ -0,0 +1,8 @@
+function Apply() {
+  alert('changer are applied');
+}
+
+function generateTableBridge(){
+  var table = document.getElementById('bridge-info-container');
+  table.innerHTML += '<table> </table>';
+}
