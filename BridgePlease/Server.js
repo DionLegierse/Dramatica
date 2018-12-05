6dcafdab930f987e021694401aee2851bc5eb4f6
Kayne Saridjo <kayne.saridjo@hotmail.com>
2018-12-05 12:51:20 +0100

    Update - BridgePlease - test script applied

--- BridgePlease/Server.js
+++ BridgePlease/Server.js
@@ -4,8 +4,11 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('<script>alert("Hello! I am an alert box!!");</script>');
 
+    var response = {'ip' : req.ip, 'message' : req.query.message, 'AAAA': req.query.int}
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(response));
+    console.log(JSON.stringify(response));
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log('Example app listening on port ${port}!'));
