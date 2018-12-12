47c95aa6bdd9aff4422987839fbbfa8580012221
Harmey <hammiekaaz@gmail.com>
2018-12-12 17:57:04 +0100

    Update - Nodes - Finished basic Json handler, Next: BorderNode handler.

--- NodesWithShields/Combi_Nodes/src/main.cpp
+++ NodesWithShields/Combi_Nodes/src/main.cpp
@@ -21,24 +21,11 @@ painlessMesh  mesh;
 //struct to configure inputs/outputs
 struct NodeInfo
 {
-    int address = 0;
-    int ledOne[2] = {0, 0};
-    int ledTwo[2] = {0, 0};
-    int button[2] = {0, 0};
+    uint32_t address = 0;
+    int button[2][2] = { {0, 0}, {0, 0} };
 };
 
 NodeInfo nodeInfo[100];
-
-// User stub
-void sendMessage() ; // Prototype so PlatformIO doesn't complain
-void sendButton();
-
-void sendMessage()
-{
-    String msg = "Hello from node ";
-    msg += mesh.getNodeId();
-    mesh.sendBroadcast( msg );
-}
 
 // Needed for painless library
 void receivedCallback( uint32_t from, String &msg )
@@ -46,15 +33,8 @@ void receivedCallback( uint32_t from, String &msg )
     Serial.printf( "startHere: Received from %u msg=%s\n", from, msg.c_str() );
     /* Json format
     {
-    "address": 123412,
-
-    "buttonOne": 0/1,
-    "greenOne": 0/1,
-    "redOne": 0/1,
-
-    "buttonTwo": 0/1,
-    "greenTwo": 0/1,
-    "redTwo": 0/1
+    	"Address": 123412,
+    	"Button": [[0, 0], [0, 0]]
     }
     */
     DynamicJsonBuffer jsonBuffer;
@@ -67,25 +47,23 @@ void receivedCallback( uint32_t from, String &msg )
             n++;
         }
 
-
         //bind button one to the 2 leds
-        nodeInfo[n].address = root["address"];
-        nodeInfo[n].button[0] = root["buttonOne"];
-        nodeInfo[n].ledOne[0] = root["redOne"];
-        nodeInfo[n].ledOne[1] = root["greenOne"];
-
-        //bind button two to the 2 leds
-        nodeInfo[n].button[1] = root["buttonTwo"];
-        nodeInfo[n].ledTwo[0] = root["redTwo"];
-        nodeInfo[n].ledTwo[1] = root["greenTwo"];
+        nodeInfo[n].address = root["Address"];
+        for (size_t i = 0; i < 2; i++)
+        {
+            for (size_t k = 0; k < 2; k++)
+            {
+                nodeInfo[n].button[k][i] = root["Button"][k][i];
+            }
+        }
     }
     else
     {
-    Serial.println("Root error, no json detected!");
+        Serial.println("Root error, no json detected!");
     }
 
   //toggle led 0
-  if (strcmp(msg.c_str(), "0") == 0)
+  if (strcmp(msg.c_str(), "GreenToggle") == 0)
   {
     if (ledOn[0])
       ledOn[0] = !ledOn[0];
@@ -98,7 +76,7 @@ void receivedCallback( uint32_t from, String &msg )
       digitalWrite(led[0], LOW);
   }
   //toggle led 1
-    if (strcmp(msg.c_str(), "1") == 0)
+    if (strcmp(msg.c_str(), "RedToggle") == 0)
     {
         if (ledOn[1])
             ledOn[1] = !ledOn[1];
@@ -156,8 +134,16 @@ void loop()
         //read button 0 and turn on/off led 0
         if (digitalRead(button[i]) == LOW && allowButton[i])
         {
-            String msg = "0";
-            mesh.sendBroadcast(msg);
+            if (nodeInfo[i].button[i][0] == 1)
+            {
+                String msg = "GreenToggle";
+                mesh.sendSingle(nodeInfo[i].address, msg);
+            }
+            if (nodeInfo[i].button[i][1] == 1)
+            {
+                String msg = "RedToggle";
+                mesh.sendSingle(nodeInfo[i].address, msg);
+            }
             allowButton[i] = !allowButton[i];
         }
         else if (digitalRead(button[i]) == HIGH && !allowButton[i])
