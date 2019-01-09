5f8ae109ed516ed486511c03c3664f505bcb4f4d
djjansse2 <djjansse2@avans.nl>
2019-01-09 12:44:16 +0100

    Update - Just a commit

--- Theapp/app/src/main/assets/network/network.js
+++ Theapp/app/src/main/assets/network/network.js
@@ -5,13 +5,7 @@ function goBackHome(){
 function sendHTTPRequest() {
     console.log("Attempting to send request");
     var xmlHttp = new XMLHttpRequest();
-    /*
-    xmlHttp.onreadystatechange = function() {
-            if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-                callback(xmlHttp.responseText);
-        }
-        */
-    xmlHttp.open( "GET", "http://pi:3000/?message=AllesInTypenWatJeWilt&int=69", true );
-    xmlHttp.send();
+    xmlHttp.open( "GET", "http://192.168.11.14:3000?message=ProjectDomotica", true );
+    xmlHttp.send( null );
     console.log(xmlHttp.responseText);
 }
