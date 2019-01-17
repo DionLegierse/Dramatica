36a3a4c200c2b89238b9250d23a4d4f6eea6baff
djjansse2 <djjansse2@avans.nl>
2019-01-17 10:52:37 +0100

    Update - TheaPP - Added small delay between two requests

--- TheaPP_Android/app/src/main/assets/javascript/jsonfunctions.js
+++ TheaPP_Android/app/src/main/assets/javascript/jsonfunctions.js
@@ -1,7 +1,13 @@
 var nodes;
+var timer;
 
 function fetchList()
 {
+    sendHTTPRequest('{"ADD":0,"CMD":"GET","ARG":0}');
+    while( timer < 1000 ) {
+        timer++
+    }
+    timer = 0;
     sendHTTPRequest('{"ADD":0,"CMD":"GET","ARG":0}');
 }
 
